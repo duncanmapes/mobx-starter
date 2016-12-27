e226563d4b95cf5a48937732444f82395907f14a
Duncan Mapes <duncan.mapes@challengemediagroup.com>
2016-12-27 15:08:52 -0500

    initial create of personal mobx starter

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserPlugin({ url: 'http://localhost:3000' })
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+};
